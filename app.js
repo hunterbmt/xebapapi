3ce464285a34eb7ddb4e34e7198f1292dd1a68f4
HunterBMT <hunterbmt@github>
2014-06-11 16:51:23 +0700

    add init folder

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var app = express();
 
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  //app.use(app.router);
+  //app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var User = new Schema({
+  _id: ObjectId,
+  name: String
+});
+
+var UserModel = mongoose.model('User', User);
+var instance = new UserModel();
+instance.name = "hunter";
+mongoose.connect('mongodb://localhost/test');
+
 app.get('/', function(req, res) {
-  res.send('Chào xe bắp');
+	instance.save(function(err){
+		res.send('Chào xe bắp');
+	})
 });
 
 var server = app.listen(3000, function() {
